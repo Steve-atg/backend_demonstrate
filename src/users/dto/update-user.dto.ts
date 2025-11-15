e70aa3ae7aa7e8064fe616e7f9681d56c74830f3
Steve-atg <95837635+Steve-atg@users.noreply.github.com>
2025-11-16 00:15:12 +0800

    feat(users): refactor UpdateUserDto to include detailed user properties and remove email field

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,54 @@
-import { PartialType, OmitType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Gender } from '@prisma/client';
+import {
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsEnum,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['email']),
-) {}
+export class UpdateUserDto {
+  @ApiProperty({
+    description: 'The username of the user',
+    example: 'john_doe',
+  })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @ApiProperty({
+    description: 'The password for the user account',
+    example: 'securePassword123',
+    minLength: 6,
+  })
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @ApiPropertyOptional({
+    description: 'URL to the user avatar image',
+    example: 'https://example.com/avatar.jpg',
+  })
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+
+  @ApiPropertyOptional({
+    description: 'The gender of the user',
+    enum: Gender,
+    example: Gender.M,
+  })
+  @IsOptional()
+  @IsEnum(Gender)
+  gender?: Gender;
+
+  @ApiPropertyOptional({
+    description: 'The date of birth of the user',
+    example: '1990-01-01T00:00:00.000Z',
+    type: Date,
+  })
+  @IsOptional()
+  dateOfBirth?: Date;
+}
